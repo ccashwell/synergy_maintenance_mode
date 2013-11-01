6636b041cd3bedc992368265ee5d1514f634e761
Jeremy Merrill <jeremy.merrill@sparkdesign.com>
2013-11-01 11:54:29 -0700

    have maintenance page default to maint.html

--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -1,4 +1,4 @@
 Spree::AppConfiguration.class_eval do
   preference :maintenance_mode, :boolean, :default => false
-  preference :maintenance_mode_page, :string, :default => '500.htm'
+  preference :maintenance_mode_page, :string, :default => 'maint.html'
 end
