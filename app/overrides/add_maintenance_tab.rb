8fc899fe826c31005578764518e84075f0e307a0
Chris Cashwell <ccashwell@gmail.com>
2015-08-06 09:42:40 -0700

    Update add_maintenance_tab.rb

--- app/overrides/add_maintenance_tab.rb
+++ app/overrides/add_maintenance_tab.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path  => "spree/layouts/admin",
                      :name          => "add_maintenance_tab",
                      :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text          => "<%= tab(:maintenance, :url => '/admin/maintenance') %>")
+                     :text          => "<li><a href='/admin/maintenance'>Maintenance Mode</a></li>")
